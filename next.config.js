1171283b655cd8b67717eac00b73f957a9e88c31
VIKRAM <131445257+VIKRAM7-AV@users.noreply.github.com>
2025-03-20 18:55:27 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['res.cloudinary.com','randomuser.me'],
-      },
-      experimental:{
-        reactRoot: true,
-        suppressHydrationWarning: true,
-      }
-}
+  images: {
+    domains: ['res.cloudinary.com', 'randomuser.me'],
+  },
+  experimental: {
+    reactRoot: true,
+    suppressHydrationWarning: true,
+  },
+  output: 'export', // Enables static export
+  trailingSlash: true, // Ensures proper routing for static deployment
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
